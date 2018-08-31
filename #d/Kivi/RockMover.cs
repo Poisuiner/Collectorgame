7dbe5f514efeef904a08cf13a81198c4a1224bdb
Anziiiii2 <36483869+Anziiiii2@users.noreply.github.com>
2018-08-31 09:47:23 +0300

    Add files via upload

---
+++ #d/Kivi/RockMover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockMover : MonoBehaviour
+{
+
+    public float delta = 30f;  
+    public float speed = 10.0f;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = startPos;
+        v.z += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}

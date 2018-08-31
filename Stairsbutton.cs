df329ddbd0074c75e64ef988cada605617993dcf
Poisuiner <42261884+Poisuiner@users.noreply.github.com>
2018-08-31 09:11:51 +0300

    Add files via upload

---
+++ Stairsbutton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stairsbutton : MonoBehaviour {
+
+    public GameObject Stairs;
+    int i = 0 ;
+
+    void OnTriggerEnter(Collider col)
+    {
+        while (i < 1)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                Instantiate(Stairs, new Vector3(-6, -1, 12), Quaternion.identity);
+                i++;
+            }
+        }
+        
+
+        
+
+        
+    }
+
+}

ae6f4061982296042505c2087f983a95a8342e5e
Anziiiii2 <36483869+Anziiiii2@users.noreply.github.com>
2018-10-01 09:03:41 +0300

    Add files via upload

---
+++ KolikkoSpawneri/Coin.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin : MonoBehaviour {
+
+    public static int CoinCount = 0;
+    public static int vic=0;
+    void Awake()
+        {
+            ++Coin.CoinCount;
+        }
+    public void OnTriggerEnter(Collider Col)
+        {
+        Debug.Log(Col);
+         if (!Col.CompareTag("SpawnArea"))
+            {
+            Destroy(gameObject); 
+            }
+        if (Col.CompareTag("Player"))
+        {
+            Destroy(gameObject);    
+        }   
+        }
+
+    void OnDestroy()
+    {
+        --Coin.CoinCount;
+
+        if (Coin.CoinCount <= 0)
+        {
+            GameObject Tim = GameObject.Find("LevelTimer");
+            Destroy(Tim);
+
+            GameObject[] FireworkSystems =
+            GameObject.FindGameObjectsWithTag("Fireworks");
+            foreach (GameObject GO in FireworkSystems)
+            GO.GetComponent<ParticleSystem>().Play();
+            vic++;
+        }
+    }
+   
+
+}
---
+++ KolikkoSpawneri/Tehdaankolikoita.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tehdaankolikoita : MonoBehaviour {
+    public GameObject coin2;
+    Vector3 coinposition;
+    bool jatketaan = true;
+    int i;
+    public float x1;
+    public float x2;
+    public float y1;
+    public float z1;
+    public float z2;
+
+    void Start() {
+        CheckCoin();
+    }
+    public void CheckCoin() {
+        do
+        {
+            if (i <= 10)
+            {
+                coinposition = new Vector3(Random.Range(x1, x2), y1, Random.Range(z1, z2));
+                Instantiate(coin2, coinposition, Quaternion.identity);
+                i++;          
+            }
+            else if( i > 10)
+            { jatketaan = false; }
+            
+        } while (jatketaan == true); 
+    }
+}
